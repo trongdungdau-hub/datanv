99294405c3b1927cbaca34690c08277b97bbbcd0
trongdungdau-hub <trongdungdau@gmail.com>
2026-03-05 20:00:05 +0700

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,8 +1,11 @@
+// Link Google Apps Script
 const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxWaVZbc24TCQHZoowqhW9tTEf5gtewczPfLMYrKMqDKLfjT9C3jUPYsQyXdNpCRleA/exec";
-let empData = []; 
-let currentIndex = -1; 
-let currentPDFBytes = null;
-let exportedList = JSON.parse(localStorage.getItem('exportedData') || '{}');
-let croppedImages = { '45': { blob: null, original: null }, '46': { blob: null, original: null }, '47': { blob: null, original: null }, '48': { blob: null, original: null }, '49': { blob: null, original: null } };
-let cropper = null; 
-let currentCropField = null;
+
+// Ép biến thành toàn cục (Global) để các file JS khác đều dùng chung được
+window.empData = []; 
+window.currentIndex = -1; 
+window.currentPDFBytes = null;
+window.exportedList = JSON.parse(localStorage.getItem('exportedData') || '{}');
+window.croppedImages = { '45': { blob: null, original: null }, '46': { blob: null, original: null }, '47': { blob: null, original: null }, '48': { blob: null, original: null }, '49': { blob: null, original: null } };
+window.cropper = null; 
+window.currentCropField = null;
