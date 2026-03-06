07a49938ca2ef740b2cd40bb93b5762b4e691f43
trongdungdau-hub <trongdungdau@gmail.com>
2026-03-06 08:30:28 +0700

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,11 +1,9 @@
-// Link Google Apps Script
-window.SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxWaVZbc24TCQHZoowqhW9tTEf5gtewczPfLMYrKMqDKLfjT9C3jUPYsQyXdNpCRleA/exec";
-
-// Biến toàn cục
-window.empData = []; 
-window.currentIndex = -1; 
-window.currentPDFBytes = null;
-window.exportedList = JSON.parse(localStorage.getItem('exportedData') || '{}');
-window.croppedImages = { '45': { blob: null, original: null }, '46': { blob: null, original: null }, '47': { blob: null, original: null }, '48': { blob: null, original: null }, '49': { blob: null, original: null } };
-window.cropper = null; 
-window.currentCropField = null;
+// Dùng 'var' để tất cả các file khác đều gọi được dữ liệu này
+var SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxWaVZbc24TCQHZoowqhW9tTEf5gtewczPfLMYrKMqDKLfjT9C3jUPYsQyXdNpCRleA/exec";
+var empData = []; 
+var currentIndex = -1; 
+var currentPDFBytes = null;
+var exportedList = JSON.parse(localStorage.getItem('exportedData') || '{}');
+var croppedImages = { '45': { blob: null, original: null }, '46': { blob: null, original: null }, '47': { blob: null, original: null }, '48': { blob: null, original: null }, '49': { blob: null, original: null } };
+var cropper = null; 
+var currentCropField = null;
